97911fa3f1efaf5b70a5d66753a45ba3ff93b652
Štěpán Votava <61780413+stepan-votava@users.noreply.github.com>
2021-06-08 15:09:20 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,6 +19,9 @@ let package = Package(
         .library(
             name: "WultraSSLPinning",
             targets: ["WultraSSLPinning", "PowerAuth2"]),
+        .library(
+            name: "AppProtection",
+            targets: ["AppProtection"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -30,6 +33,7 @@ let package = Package(
         .binaryTarget(name: "PowerAuth2", path: "Sources/PowerAuth2.xcframework"),
         .binaryTarget(name: "PowerAuth2ForExtensions", path: "Sources/PowerAuth2ForExtensions.xcframework"),
         .binaryTarget(name: "PowerAuth2ForWatch", path: "Sources/PowerAuth2ForWatch.xcframework"),
-        .binaryTarget(name: "WultraSSLPinning", path: "Sources/WultraSSLPinning.xcframework"),        
+        .binaryTarget(name: "WultraSSLPinning", path: "Sources/WultraSSLPinning.xcframework"),
+        .binaryTarget(name: "AppProtection", path: "Sources/AppProtection.xcframework"),
     ]
 )
